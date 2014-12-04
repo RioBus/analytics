b375cf9e933061f4177d743e9c9a02bcc94eff78
vitor-costa <v2costa@gmail.com>
2014-12-04 13:50:19 -0200

    api to get old jsons

--- report.js
+++ report.js
@@ -2,19 +2,42 @@ var express = require('express')
 var fs = require('fs')
 var app = express()
 
-app.get('/', function (req, res) {
+app.get('/api/:min_date/:max_date', function (req, res) {
   var min_date = req.param('min_date'); // YYYMMDDHH
   var max_date = req.param('max_date'); // YYYMMDDHH
 
-  var allfiles = fs.readdirSync(__dirname + "/files/").sort();
+  console.log(min_date + "-" + max_date);
+
+  var allFiles = fs.readdirSync(__dirname + "/files/").sort();
+  console.log(allFiles);
   var files_to_use = [];
-  var l = allfiles.length;
-  var response = "";
+  var l = allFiles.length;
+
+  console.log(l);
+
+  // removes the .json from file name
+  var allFilesName = allFiles.map(function(str) {
+    return str.split(".")[0];
+  });
+
+  var response = "[";
   for (var i = 0; i < l; i++) {
-  	if (allfiles[i] >= min_date && allfiles[i] <= max_date) {
-  		response += fs.readFileSync(__dirname + "/files/" + allfiles[i]) + "\n";
+  	if (allFilesName[i] >= min_date && allFilesName[i] <= max_date) {
+      
+      // concatenates ',' for next json
+      if (response != "[") {
+        response += ",";
+      }
+
+  		response += fs.readFileSync(__dirname + "/files/" + allFiles[i]) + "\n";
   	}
   };
+  response += "]";
 
+  res.send(response);
 
 })
+
+app.use(express.static(__dirname + '/public'));
+
+var server = app.listen(3002);
